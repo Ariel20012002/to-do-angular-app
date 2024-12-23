dd243882f1eb664c7a7aa9b71d5a057e746743c4
Ariel20012002 <ariel20012002@gmail.com>
2024-12-23 01:09:12 -0500

    Se removio solo una linea de codigo que es el paquete.

--- src/app/domain/usercase/add-tarea-use-case.ts
+++ src/app/domain/usercase/add-tarea-use-case.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { TareaGateway } from "../models/Tarea/gateway/tarea-gateway";
 import { Observable } from "rxjs";
 import { AddTarea } from "../models/Tarea/add-tarea.model";
-import { Tarea } from "../models/Tarea/tarea";
 
 @Injectable({
   providedIn: 'root'
